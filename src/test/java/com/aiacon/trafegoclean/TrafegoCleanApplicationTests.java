796f6887fbb771aa61842a70a3c4a64dde989f50
Lucas Oliveira <lucazolvr@gmail.com>
2025-04-14 01:34:44 -0300

    commit inicial

---
+++ src/test/java/com/aiacon/trafegoclean/TrafegoCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiacon.trafegoclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafegoCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
